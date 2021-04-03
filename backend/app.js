2fd4feb515a307dfabf2a9047a05a869730a531d
Alice Hedmark <mothlym@hotmail.com>
2021-04-03 12:33:10 +0200

    better mysql connection handling and support for serve by nodejs

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,27 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
+app.use(express.static("../frontend/dist/datapresentation/"));
 
 app.use('/api/v1/solar_panel', solarPanel);
+
+
+app.get('/', getRoot);
+app.get('/*', getUndefined);
+
+
+function getRoot(request, response) {
+    response.sendFile(path.resolve('../frontend/dist/datapresentation/index.html'));
+}
+
+function getUndefined(request, response) {
+    response.sendFile(path.resolve('../frontend/dist/datapresentation/index.html'));
+}
 
 module.exports = app;
--- backend/routes/solar-panel.js
+++ backend/routes/solar-panel.js
@@ -3,39 +3,57 @@ var mysql = require('mysql')
 var router = express.Router();
 var config = require('../secure/config');
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
     host: '192.168.1.82',
     user: 'root',
     password: config.solpanelPw,
     database: 'solpanelen'
-})
-
-connection.connect();
+});
 
 router.get('/', function(req, res, next) {
-    connection.query('SELECT * FROM temp ORDER BY Datum DESC LIMIT 1;', function(err, rows, fields) {
+    pool.getConnection(function(err, connection) {
         if (err) {
             console.log(err);
+            callback(true);
+            return;
         }
-        res.json({ date: rows[0].Datum, tank: rows[0].TT, in: rows[0].TI, out: rows[0].TO, panel: rows[0].TP, pump: rows[0].pump })
-    });
+        var sql = 'SELECT * FROM temp ORDER BY Datum DESC LIMIT 1;';
+        connection.query(sql, [], function(err, rows) {
+            connection.release();
+            if (err) {
+                console.log(err);
+                callback(true);
+                return;
+            }
+            res.json({ date: rows[0].Datum, tank: rows[0].TT, in: rows[0].TI, out: rows[0].TO, panel: rows[0].TP, pump: rows[0].pump })
+        });
+    })
+
 });
 
 router.get('/interval', function(req, res, next) {
     var from = req.query.from;
     var to = req.query.to;
 
-    connection.query(
-        'SELECT * FROM temp WHERE Datum between ' +
-        mysql.escape(from) +
-        ' AND ' +
-        mysql.escape(to) + ' ORDER BY Datum;',
-        function(err, rows, fields) {
-            if (err) {
-                console.log(err);
-            }
-            res.json(extractPanelDataArr(rows))
-        });
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.log(err);
+            callback(true);
+            return;
+        }
+        var sql = 'SELECT * FROM temp WHERE Datum between ' + mysql.escape(from) + ' AND ' + mysql.escape(to) + ' ORDER BY Datum;'
+        connection.query(
+            sql, [],
+            function(err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                    callback(true);
+                    return;
+                }
+                res.json(extractPanelDataArr(rows))
+            });
+    })
 });
 
 function extractPanelDataArr(rows) {
